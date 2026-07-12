a75cce22253855ba64c9b6ea0a6e8c5f90106818
JL <115737916+yikesboy@users.noreply.github.com>
2026-07-12 15:15:54 +0200

    build(nix): switch to stable 26.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-26.05";
   };
 
   outputs = { self, nixpkgs }: {

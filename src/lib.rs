766acc618558e437b54cf20179ee27f6cd1778da
JL <115737916+yikesboy@users.noreply.github.com>
2026-07-14 15:40:09 +0200

    feat: patchwise loads in neovim

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,13 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use api::types::LogLevel;
+use nvim_oxi as oxi;
+use oxi::{Dictionary, api};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+const PLUGIN_LOGLEVEL: LogLevel = LogLevel::Info;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[oxi::plugin]
+fn patchwise() -> Dictionary {
+    api::notify("Patchwise loaded", PLUGIN_LOGLEVEL, &Default::default())
+        .expect("failed to send notification");
+
+    Dictionary::new()
 }
